21e7364bba23f45af1ad720bee4960bfb50828cb
Roberto Paes <robertocpaes@gmail.com>
2022-09-24 21:21:59 -0300

    -

---
+++ MinecraftServer.Api/make.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+sudo apt-get install jq
+CONFIG_FILE="appsettings.Production.json"
+tmp=$(mktemp)
+jq '.ConnectionStrings.Redis = "'$REDIS_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+jq '.MongoConnections.CollectionSettings[0].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+jq '.MongoConnections.CollectionSettings[1].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+jq '.MongoConnections.CollectionSettings[2].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
