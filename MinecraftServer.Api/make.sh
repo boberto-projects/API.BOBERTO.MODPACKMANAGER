079bc3dd83bc5eefc314397cea159d99ae0fced6
Roberto Paes <robertocpaes@gmail.com>
2022-09-25 00:12:00 -0300

    fix deploy

--- MinecraftServer.Api/make.sh
+++ MinecraftServer.Api/make.sh
@@ -1,4 +1,5 @@
-﻿#!/usr/bin/env bash
+﻿#!/bin/sh
+echo "Criando appsettings..."
 apt update -y
 apt install jq -y
 CONFIG_FILE="appsettings.Production.json"
@@ -8,4 +9,5 @@ jq '.MongoConnections.CollectionSettings[0].ConnectionString = "'$MONGO_URL'"' $
 jq '.MongoConnections.CollectionSettings[1].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
 jq '.MongoConnections.CollectionSettings[2].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
 apt remove jq -y
+echo "appsettings editado com sucesso"
 exit 0
