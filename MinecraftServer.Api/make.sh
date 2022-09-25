143c8acb69fb8e5e6e979d55bf497a03e19622b8
Roberto Paes <robertocpaes@gmail.com>
2022-09-24 22:23:38 -0300

    update make sh

--- MinecraftServer.Api/make.sh
+++ MinecraftServer.Api/make.sh
@@ -1,8 +1,10 @@
-﻿#!/bin/bash
-apt update && apt install jq -y
+﻿#!/bin/bash -x
+apt update -y
+apt install jq -y
 CONFIG_FILE="appsettings.Production.json"
 tmp=$(mktemp)
 jq '.ConnectionStrings.Redis = "'$REDIS_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
 jq '.MongoConnections.CollectionSettings[0].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
 jq '.MongoConnections.CollectionSettings[1].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
 jq '.MongoConnections.CollectionSettings[2].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+apt remove jq -y
