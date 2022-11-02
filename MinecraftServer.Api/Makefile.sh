1057839a328c7ae8c55d57eb4ddbad69d92e7e5f
Roberto Paes <robertocpaes@gmail.com>
2022-11-02 16:29:09 -0300

    -

--- MinecraftServer.Api/Makefile.sh
+++ MinecraftServer.Api/Makefile.sh
@@ -1,14 +1,14 @@
 ﻿#!/usr/bin/env bash
-echo "Baixando dependencias"
-echo "Testando daeawsae" >> "teste.txt"
-apt update -y
-apt install jq -y
-CONFIG_FILE="appsettings.Production.json"
-tmp=$(mktemp)
-echo "Criando appsettings... para " . $CONFIG_FILE
-jq '.ConnectionStrings.Redis = "'$REDIS_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
-jq '.MongoConnections.CollectionSettings[0].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
-jq '.MongoConnections.CollectionSettings[1].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
-jq '.MongoConnections.CollectionSettings[2].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
-apt remove jq -y
-echo "appsettings editado com sucesso"
+# echo "Baixando dependencias"
+# echo "Testando daeawsae" >> "teste.txt"
+# apt update -y
+# apt install jq -y
+# CONFIG_FILE="appsettings.Production.json"
+# tmp=$(mktemp)
+# echo "Criando appsettings... para " . $CONFIG_FILE
+# jq '.ConnectionStrings.Redis = "'$REDIS_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+# jq '.MongoConnections.CollectionSettings[0].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+# jq '.MongoConnections.CollectionSettings[1].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+# jq '.MongoConnections.CollectionSettings[2].ConnectionString = "'$MONGO_URL'"' $CONFIG_FILE > "$tmp" && mv "$tmp" $CONFIG_FILE
+# apt remove jq -y
+# echo "appsettings editado com sucesso"
--- MinecraftServer.Api/deploy.sh
+++ MinecraftServer.Api/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #configuro as variaveis
-APP="api-mine-modpack"
-PORT=5555
+# APP="api-mine-modpack"
+# PORT=5555
 
-dokku apps:destroy $APP --force && dokku apps:create $APP || dokku apps:create $APP
-dokku config:set $APP ASPNETCORE_ENVIRONMENT=Production
-dokku builder-dockerfile:set $APP dockerfile-path MinecraftServer.Api/Dockerfile
-dokku domains:add $APP 167.114.196.39
-dokku nginx:validate-config $APP --clean
-dokku proxy:ports-add $APP http:$PORT:80
-dokku redis:create redis-service
-dokku redis:link redis-service $APP
-dokku mongo:create mongo-service
-dokku mongo:link mongo-service $APP
-dokku config:show $APP
+# dokku apps:destroy $APP --force && dokku apps:create $APP || dokku apps:create $APP
+# dokku config:set $APP ASPNETCORE_ENVIRONMENT=Production
+# dokku builder-dockerfile:set $APP dockerfile-path MinecraftServer.Api/Dockerfile
+# dokku domains:add $APP 167.114.196.39
+# dokku nginx:validate-config $APP --clean
+# dokku proxy:ports-add $APP http:$PORT:80
+# dokku redis:create redis-service
+# dokku redis:link redis-service $APP
+# dokku mongo:create mongo-service
+# dokku mongo:link mongo-service $APP
+# dokku config:show $APP
 
 
 #dokku proxy:ports-add $APP http:$PORT:80
